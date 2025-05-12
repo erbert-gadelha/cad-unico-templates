9db28008de65f991c99f895111e0bb7cc09c3f31
Guilherme Calland <gui.crb0@gmail.com>
2025-05-12 18:29:15 -0300

    ajusted interocorrencia

--- scripts/cadunico-processos.py
+++ scripts/cadunico-processos.py
@@ -15,7 +15,7 @@ def generateBody(employee_id:int, location_id:int, task_id:int):
 
 def postHttp(host_:str, body) -> int:
     headers = {
-        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJyajhCd21MZDNiTU9BampXZ1FVb05wSWFnMUtkRHBGY0RMTEdxOFlxZXlzIn0.eyJleHAiOjE3NDY1ODY1NTAsImlhdCI6MTc0NjU1MDU1MCwianRpIjoiM2IyMmU0OWMtNTZiYy00MmJmLWExOWEtZDI1NmE3NDE2ZTFhIiwiaXNzIjoiaHR0cHM6Ly9pZHAuZmliYm8uY28vcmVhbG1zL2NhZHVuaWNvIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImJiMmQ2ZTA4LWJkYmQtNDM1Ny1hMDY0LTgzYzUxYzY0ZTQ5YiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImZpYmJvIiwic2Vzc2lvbl9zdGF0ZSI6ImVmOTYzNmY5LTU1YzAtNGI4ZS05MTk0LWZlYzcxM2I3OTliYSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJWSUVXX0FOQUxZVElDU19EQVNIQk9BUkQiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtY2FkdW5pY28iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJzaWQiOiJlZjk2MzZmOS01NWMwLTRiOGUtOTE5NC1mZWM3MTNiNzk5YmEiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZG9lc0l0VXNlUHJvY2Vzc0luc3RhbmNlRW1wbG95ZWUiOmZhbHNlLCJuYW1lIjoiU2xheXRob24gR2xleXNvbiIsInByZWZlcnJlZF91c2VybmFtZSI6InNsYXl0aG9uIiwiZ2l2ZW5fbmFtZSI6IlNsYXl0aG9uIiwiZmFtaWx5X25hbWUiOiJHbGV5c29uIiwiZW1haWwiOiJzbGF5dGhvbkBmaWJiby5jbyJ9.Cx34BlJJkI_KFb4mXQtvZPnmQ4Bb45fKFphJYsr0JWBprYpXSUtl4u2CuY3M6I1SHJioBchFgnYhfHFN_n_lrJZBeyrpMI_-ETp2YijC4Qis-gn7aT6bG-Yt1fD0mH6RifWGuBX_kiLKvgW6SP2ng4oPfPz2lADhdMtLAJSoBIZtvqrv9AxXimdzFE4ehY_VPRuagAw_7JklpKBY7q5D-xKC80Ms4OCWKqoirQYNm72K6FWKHy_yCb3t1Gu1tqggeEM0zwqj-FFpgg0pGWFAbW9nQAKRUKsAJV8vsEpDiPbNjycInnOnOd_gDAAdkXtHBuLZE756Z_CL1-lXDTGlSg",
+        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJyajhCd21MZDNiTU9BampXZ1FVb05wSWFnMUtkRHBGY0RMTEdxOFlxZXlzIn0.eyJleHAiOjE3NDY2NTQ2MTQsImlhdCI6MTc0NjYxODYxNCwianRpIjoiNGRlNWI1ZmQtNDU1Yi00YzMzLTlkNzktOTBjYzcxMWFmY2QxIiwiaXNzIjoiaHR0cHM6Ly9pZHAuZmliYm8uY28vcmVhbG1zL2NhZHVuaWNvIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImJiMmQ2ZTA4LWJkYmQtNDM1Ny1hMDY0LTgzYzUxYzY0ZTQ5YiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImZpYmJvIiwic2Vzc2lvbl9zdGF0ZSI6ImUzMGQ1OGU5LTU5ZWUtNGNhYi05Y2FmLWMyOTkxNjA4ZTg5MSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJWSUVXX0FOQUxZVElDU19EQVNIQk9BUkQiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtY2FkdW5pY28iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJzaWQiOiJlMzBkNThlOS01OWVlLTRjYWItOWNhZi1jMjk5MTYwOGU4OTEiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZG9lc0l0VXNlUHJvY2Vzc0luc3RhbmNlRW1wbG95ZWUiOnRydWUsIm5hbWUiOiJTbGF5dGhvbiBHbGV5c29uIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2xheXRob24iLCJnaXZlbl9uYW1lIjoiU2xheXRob24iLCJmYW1pbHlfbmFtZSI6IkdsZXlzb24iLCJlbWFpbCI6InNsYXl0aG9uQGZpYmJvLmNvIn0.uhZJb0mGAAZyBsWB17Ydp33lBbxvZwN4nK3mpmOk0XlxiGwDFs7U3FYSGn6V8eKCt69NSOwc9RAXK0AhF6NUZaJmTgTtpX8L89b-nd5hOiAy6VjFbcOJ9EQJAhyKLomt4ImgrLtRSIOiYK0LPsok4vWz3j-IgfON_e2xqlhhTCFiTaLTtURHPv7JHaml5buVuGM962_ndOo5rKOT6oTos4wINMHbtlHTXYHUG8e3KDl0cKQzN5lSEQXaQRSpAlbbnKitFfMRGVWhryF2qniMn5hNJ7y9Fsfsvth9fnmqs2FpLqAFOvwm9a6diyEpZuIbcGKXBec1DuhH6QdjiC-pxQ",
         "Content-Type": "application/json"
     }
 
@@ -30,23 +30,25 @@ def postHttp(host_:str, body) -> int:
 def instantiateLiderProcess():
     linhas = readCSV('local-usuario.csv')
     #host = f"http://0.0.0.0:8080"
-    host = f"https://api.fibbo.coaaa"
+    host = f"https://api.fibbo.coaaaa"
     process_id = 62
     task_id = 56
 
-    for i in range(1, len(linhas)):
+    # for i in range(20, len(linhas)):
+    for i in range(20, 21):
         line = linhas[i]
         body = generateBody(line[3], line[0], task_id)
 
         print(body)
-        #status = postHttp(f"{host}/process-instances/{process_id}", body)    
-        #if status >= 300:
-        #    print(f"[{status}] deu ruim, chefe.")
-        #    break
+        status = postHttp(f"{host}/process-instances/{process_id}", body)    
+        if status >= 300:
+            print("")
+            print(f"[{status}] deu ruim, chefe.")
+            break
 
 def instantiateSupervisorProcess():
     #host = f"http://0.0.0.0:8080"
-    host = f"https://api.fibbo.co"
+    host = f"https://api.fibbo.coaaaaa"
     process_id = 65
     task_id = 59
 
@@ -72,7 +74,7 @@ def instantiateSupervisorProcess():
         body = generateBody(supervisor['user'], supervisor['location'], task_id)
         status = postHttp(f"{host}/process-instances/{process_id}", body)    
         if status >= 300:
-            print(f"[{status}] deu ruim, chefe.")
+            print(f"/n/n[{status}] deu ruim, chefe./n/n")
             break
 
 
